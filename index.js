ec5fbd9b5521907b4b08998bbaa425105799f065
James Quiambao <james.quiambao@yahoo.ca>
2016-10-18 12:00:30 -0400

    Wikipedia App
    Web application to search wikipedia articles

---
+++ index.js
@@ -0,0 +1,84 @@
+// If user clicks wikipedia picture, points them to a random article
+$("#random-article").on("click", function(){
+	window.open("https://en.wikipedia.org/wiki/Special:Random");
+});
+
+// Hover text for picture
+$(document).ready(function(){
+    $('[data-toggle="tooltip"]').tooltip();
+});
+
+//If user clicks the search button
+$("#button-search").on("click", function(){
+  console.log("pie");
+  printResults();
+
+});
+
+//If user presses the enter key
+$("#search").keydown(function(event){
+    if(event.keyCode == 13){
+      event.preventDefault();
+      printResults();  
+    }
+});
+
+// Algorithm to output query to user
+function printResults(){
+
+  //Refresh results
+  $('#results').empty();
+  $('#body').css("overflow-y", "visible");
+  $(".container-fluid").css("height", "auto");
+  $('.vertical-center').css("transform", "translateY(5%)");
+  $('.vertical-center').css("height", "0%");
+
+  // Get search from user and construct a url
+	var searchResult = document.getElementById('search').value;
+  var url = "https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=" + searchResult + "&srinfo=&srprop=snippet&format=json&callback=?";
+  var output = ' ';
+
+    //API Call
+	  $.getJSON(url, function(json) {
+      
+      var getResults = document.getElementById('results');
+   
+      var list = $('ul.results');
+      $.each(json.query.search, function(i) {
+
+        var li = $('<li/>').appendTo(list);
+        setTimeout(function(){ 
+          li.addClass('show'); 
+        }, 10);
+
+
+        var outputLink = $('<a/>').attr('href', "https://en.wikipedia.org/wiki/" + json.query.search[i].title);
+        outputLink.attr('target', "_blank");
+
+        outputLink.appendTo(li);
+
+        var outputTitles = $('<h3/>').text(json.query.search[i].title);
+        outputTitles.appendTo(outputLink);
+        
+        var outputInfo = $('<p/>').text(strip(json.query.search[i].snippet));
+        outputInfo.appendTo(outputLink);
+
+      });//End .each() function
+
+      //Turn HTML into plain text (remove HTML tags)
+      function strip(html)
+      {
+      var tmp = document.createElement("DIV");
+      tmp.innerHTML = html;
+      return tmp.textContent || tmp.innerText;
+      }
+
+    });// End getJSON() function
+
+    }
+    
+
+
+
+
+
